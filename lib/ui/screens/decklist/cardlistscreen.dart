a3fb71bc0ebf42b9b765257276f215a268dc5172
Nguyen Dinh Hieu <dihieu2407@gmail.com>
2026-05-14 21:45:14 +0700

    seperate menu in deck list screen

--- lib/ui/screens/decklist/cardlistscreen.dart
+++ lib/ui/screens/decklist/cardlistscreen.dart
@@ -3,6 +3,7 @@ import 'package:totoki_extract/business/path_service.dart';
 import 'package:totoki_extract/theme/appTheme.dart';
 import 'package:totoki_extract/business/flashcard/Flashcard.dart';
 import 'package:totoki_extract/ui/screens/blankfill/blankwordscreen.dart';
+import 'package:totoki_extract/ui/screens/decklist/learnModeMenu.dart';
 import 'package:totoki_extract/ui/screens/studymode/echofuse/echofuse.dart';
 import 'package:totoki_extract/ui/screens/studymode/echomatch/echomath.dart';
 import 'package:totoki_extract/ui/screens/studymode/echospell/echospell.dart';
@@ -143,9 +144,11 @@ class _CardListScreenState extends State<CardListScreen> {
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            const Text(
+                            Text(
                               "Study Modes",
-                              style: AppTheme.bodyLargeStyle,
+                              style: AppTheme.bodyMediumStyle.copyWith(
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                             IconButton(
                               onPressed: () => setState(() => menu = false),
@@ -169,104 +172,7 @@ class _CardListScreenState extends State<CardListScreen> {
   }
 }
 
-class LearnMode extends StatelessWidget {
-  const LearnMode({super.key, required this.deckID});
 
-  final int deckID;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.symmetric(vertical: 10),
-      children: [
-        NavPageBtn(
-          label: "SRS Review",
-          icon: Icons.rate_review,
-          color: AppTheme.primaryTeal,
-          screenBuilder: () => Newwayreview(deckId: deckID),
-        ),
-        NavPageBtn(
-          label: "Blank Word",
-          icon: Icons.text_fields,
-          color: AppTheme.meanFuse,
-          screenBuilder: () => BlankWordScreen(deck_id: deckID),
-        ),
-        NavPageBtn(
-          label: "Mind Field",
-          icon: Icons.psychology,
-          color: AppTheme.mindField,
-          screenBuilder: () => MindFeild(deckID: deckID),
-        ),
-        NavPageBtn(
-          label: "Word Snap",
-          icon: Icons.touch_app,
-          color: AppTheme.wordSnap,
-          screenBuilder: () => WordSnap(deck_id: deckID),
-        ),
-        NavPageBtn(
-          label: "Phoneme Mix",
-          icon: Icons.graphic_eq,
-          color: AppTheme.phoneMix,
-          screenBuilder: () => PhoneMix(deckID: deckID),
-        ),
-        NavPageBtn(
-          label: "Synonym Field",
-          icon: Icons.compare_arrows,
-          color: AppTheme.meanFuse,
-          screenBuilder: () => Synonymfield(deckID: deckID),
-        ),
-        NavPageBtn(
-          label: "Echo Spell",
-          icon: Icons.hearing,
-          color: AppTheme.echoSpell,
-          screenBuilder: () => Echospell(deck_id: deckID),
-        ),
-        NavPageBtn(
-          label: "Echo Match",
-          icon: Icons.extension,
-          color: AppTheme.echoMatch,
-          screenBuilder: () => EchoMatch(deck_id: deckID),
-        ),
-        NavPageBtn(
-          label: "Echo Fuse",
-          icon: Icons.merge_type,
-          color: AppTheme.echoFuse,
-          screenBuilder: () => EchoFuse(deck_id: deckID),
-        ),
-        NavPageBtn(
-          label: "Sound - Sight",
-          icon: Icons.visibility,
-          color: AppTheme.soundSight,
-          screenBuilder: () => SoundNSight(deck_id: deckID),
-        ),
-        NavPageBtn(
-          label: "Neuro Pick",
-          icon: Icons.image_search,
-          color: AppTheme.neuroPick,
-          screenBuilder: () => NeuroPick(deckID: deckID),
-        ),
-        NavPageBtn(
-          label: "Word Pulse",
-          icon: Icons.favorite,
-          color: AppTheme.wordPulse,
-          screenBuilder: () => WordPulse(deck_id: deckID),
-        ),
-        NavPageBtn(
-          label: "Synonym Pick",
-          icon: Icons.low_priority,
-          color: AppTheme.meanFuse,
-          screenBuilder: () => Synonympick(deckID: deckID),
-        ),
-        NavPageBtn(
-          label: "Speech Word",
-          icon: Icons.mic,
-          color: AppTheme.primaryRed,
-          screenBuilder: () => Speechword(deck_id: deckID),
-        ),
-      ],
-    );
-  }
-}
 
 class NavPageBtn extends StatelessWidget {
   final String label;
